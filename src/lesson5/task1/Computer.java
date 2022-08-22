2e92ef50f2f0e7250f4012ae8c4d43914e8a5277
Pavel Malygin <malyginpa0@gmail.com>
2022-08-23 00:16:02 +0700

    task2

---
+++ src/lesson5/task1/Computer.java
@@ -0,0 +1,20 @@
+package lesson5.task1;
+
+
+public class Computer {
+
+    static boolean isOn;
+    static boolean cpu;
+    static boolean ram;
+    static boolean hdd;
+    static int resource;
+    public static String computerDescription;
+
+
+
+    public static String DescribeComputer(boolean isOn, boolean cpu, boolean ram, boolean hdd, int resource){
+
+    return computerDescription;
+    }
+
+}
